0222dd5575571dcdfeada50eb1d12f7ddbeb47dc
Adrián <adriannegrinmartin95@gmail.com>
2018-12-17 20:01:21 +0000

    Kata2 Version 3

---
+++ src/Histogram.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Histogram {
+
+    private final int[] DATA;
+
+    public Histogram(int[] data) {
+        DATA = data;
+    }
+
+    public int[] getDATA() {
+        return DATA;
+    }
+
+    public Map<Integer, Integer> getHistogram() {
+        Map<Integer, Integer> histogram = new HashMap<>();
+        for (int value : DATA) {
+            histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
+        }
+        return histogram;
+    }
+}
--- src/kata2.java
+++ src/kata2.java
@@ -1,18 +1,14 @@
-import java.util.HashMap;
 import java.util.Map;
 
 public class kata2 {
 
     public static void main(String[] args) {
-        int[] data = {1, 1, 1, 100, 2, 4, 5, 6, 8, 8, 4, 5, 6, 2, -4};
-        Map<Integer, Integer> histogram = new HashMap<>();
 
-        for (int value : data) {
-            histogram.put(value, histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
-        }
+        Histogram histo = new Histogram(new int[]{1, 1, 1, 100, 2, 4, 5, 6, 8, 8, 4, 5, 6, 2, -4});
+        Map<Integer, Integer> histogr = histo.getHistogram();
 
-        for (int key : histogram.keySet()) {
-            System.out.println(key + " ==> " + histogram.get(key));
+        for (int key : histogr.keySet()) {
+            System.out.println(key + " ==> " + histogr.get(key));
         }
     }
 }
